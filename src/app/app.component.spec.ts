1510d4a64dacda7d2df368d16f33d211bf73781b
Ansen123 <ansenfp@gmail.com>
2022-10-10 23:13:33 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Courseapp'`, () => {
+  it(`should have as title 'courseapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Courseapp');
+    expect(app.title).toEqual('courseapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Courseapp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('courseapp app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewcourseComponent } from './viewcourse/viewcourse.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+const appRoute:Routes = [
+  {
+    path:"",component:AddcourseComponent
+  },
+  {
+    path:"viewall",component:ViewcourseComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcourseComponent,
+    ViewcourseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
