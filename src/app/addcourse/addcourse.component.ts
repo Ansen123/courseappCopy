cec53f26a96f3d8d48e664bc6b9046a183e417f9
Ansen123 <ansenfp@gmail.com>
2022-10-13 14:52:34 +0530

    Forms

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -13,6 +13,16 @@ export class AddcourseComponent implements OnInit {
   courseDuration=""
   courseDate=""
   courseVenue=""
+  readValues=()=>{
+   let data={
+    "courseTitle":this.courseTitle,
+  "courseDescription":this.courseDescription,
+  "courseDuration":this.courseDuration,
+  "courseDate":this.courseDate,
+  "courseVenue":this.courseVenue
+   }
+   console.log(data)
+  }
   ngOnInit(): void {
   }
 
